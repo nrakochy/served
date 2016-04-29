95f85e202ff61e39747151bb027e8747308cd514
nrakochy <nick.rakochy@gmail.com>
2016-04-29 12:35:19 -0500

    README. Added http codes map

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject served "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Exploration of Ring"
+  :url "http://example.com/nothing-to-see-here"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
--- src/served/response.clj
+++ src/served/response.clj
@@ -1,6 +1,31 @@
 (ns served.response)
 
+(def codes 
+  {
+    :ok 200
+    :no-content 204
+    :found 302    
+    :created 303    
+    :not-modified 304    
+    :bad-request 400
+    :forbidden 403
+    :not-found 404
+    :method-not-allowed 405
+    :server-error 500 
+  }
+)
+
+(defn add-status [resp status]
+)
+  
+
+(defn set-headers [] 
+)
+
+(defn set-body [])
+
 (defn respond [request]
-  {:status 200
+  {:status (get codes :ok)  
    :headers {"Content-Type" "text/html"}
-   :body  request})
+   :body request})
+

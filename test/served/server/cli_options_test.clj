28deec2a8416707cdda1a5523b43065edd02cec3
nrakochy <nick.rakochy@gmail.com>
2016-05-09 16:57:53 -0500

    Added tests to check results of wrongly formatted cli options

--- test/served/server/cli_options_test.clj
+++ test/served/server/cli_options_test.clj
@@ -5,6 +5,18 @@
 (deftest set-options-return-values
   (testing "#set-options"
    (testing "default port value" 
-    (is (= {:port 5000} (set-options []))
-))))
+     (is (= {:port 5000} (set-options []))))
+   (testing "command line arguments" 
+     (testing "correctly formatted arg" 
+       (let [arg (seq '("-p" "7000"))]
+       (is (= {:port 7000} (set-options arg)))))
+
+     (testing "correctly formatted custom arg with nonesense arg correctly returns custom arg" 
+     (let [arg (seq '("-p" "7000" "-nothing" "TEST"))]
+       (is (= {:port 7000} (set-options arg)))))
+
+     (testing "nonesense arg returns default arg" 
+     (let [arg (seq '("-nothing" "TEST"))]
+       (is (= {:port 5000} (set-options arg)))))
+)))
 

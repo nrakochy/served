cba484547f9bd204b143782d643e8f83ddb2a204
nrakochy <nick.rakochy@gmail.com>
2016-05-11 15:21:29 -0500

    Valid-path? check + specs

--- src/served/app/response.clj
+++ src/served/app/response.clj
@@ -1,46 +1,26 @@
 (ns served.app.response)
 
-(def codes 
-  {
-    :ok 200
-    :no-content 204
-    :found 302    
-    :created 303    
-    :not-modified 304    
-    :bad-request 400
-    :forbidden 403
-    :not-found 404
-    :method-not-allowed 405
-    :server-error 500 
-  }
-)
-
-(def media-types 
-   {
-   "htm"    "text/html"
-   "html"   "text/html"
-   "json"   "application/json"
-   "text"   "text/plain"
-  }
-)
-
-(defn set-status [resp status]
-)
-  
-
-(defn set-headers [] 
-)
-
-(defn set-body [])
-
 (defn process-request [request]
   {:status 200 
    :headers {"Content-Type" "text/html"}
+   :body (str (type request))})
+
+(defn not-found [request]
+  {:status 404 
+   :headers {"Content-Type" "text/html"}
    :body request})
 
-(defn respond [request]
-  (process-request request)
+(defn paths [routes]
+  (map :path routes)   
 )
 
+(defn valid-path? [request routes-map] 
+  (let [requested-uri (get request :uri)]
+  (.contains (paths routes-map) requested-uri))
+)
 
-
+(defn respond [request routes-map]
+  (if (valid-path? request routes-map)
+    (process-request request)
+    (not-found request))
+)
--- test/served/app/response_test.clj
+++ test/served/app/response_test.clj
@@ -6,7 +6,25 @@
   (testing "returns a sequence of :path keys"
     (let [routes [{:test "test" :path "/"} {:test1 "test1" :path "/test1"}]] 
     (let [result (lazy-seq '("/" "/test1"))]
-    (println (str result))
     (is (= result (paths routes)))))
   )
 )
+
+(deftest valid-path?-test-true 
+  (testing "returns true if request uri is included in routes-map"
+    (let [routes [{:test "test" :path "/"} {:test1 "test1" :path "/test1"}]] 
+    (let [request  {:uri "/test1" :server-name "localhost"} ] 
+    (is (= true (valid-path? request routes)))))
+  )
+)
+
+(deftest valid-path?-test-false 
+  (testing "returns false if request uri is included in routes-map"
+    (let [routes [{:test "test" :path "/"} {:test1 "test1" :path "/test1"}]] 
+    (let [request  {:uri "/missing" :server-name "localhost"} ] 
+    (is (= false (valid-path? request routes)))))
+  )
+)
+
+
+
